6c95747e7d27e3d043ad840c3b55079668063af6
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-06-29 18:37:52 +0900

    GunController 스크립트 작성 중
    간단한 발사 상호작용까지만 작성 완료.
    현재 Holder에 배정된 HandController와 GunController가 충돌하는 문제 발생.

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    [SerializeField] private Gun currentGun;
+
+    private float currentFireRate; //현재 연사 속도. 1초에 1씩 감소하고 0이 되면 발사
+
+    void Update()
+    {
+        GunFireRateCalc();
+        TryFire();
+    }
+
+    private void GunFireRateCalc() //발사 속도 계산용
+    {
+        if (currentFireRate > 0)
+            currentFireRate -= Time.deltaTime; // 대략 60분의 1 = 1
+    }
+
+    private void TryFire() //발사 시도
+    {
+        if (Input.GetButton("Fire1") && currentFireRate <= 0)
+        {
+            Fire();
+        }
+    }
+
+    private void Fire() //발사 과정
+    {
+        currentFireRate = currentGun.fireRate;
+        Shoot();
+    }
+
+    private void Shoot() //진짜 총알 쏨
+    {
+        Debug.Log("총알 발사");
+    }
+}
