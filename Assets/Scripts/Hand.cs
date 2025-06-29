58a42586b8eb1436f730f22477dd0b652db4125e
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-06-29 10:04:53 +0900

    Hand 스크립트 생성하고 기초적인 변수 인스펙터에서 설정

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -2,15 +2,14 @@ using UnityEngine;
 
 public class Hand : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public string handName; //너클이나 맨손을 구분
+    public float range; //공격 범위
+    public int damage; //공격력
+    public float workSpeed; //작업 속도
+    public float attackDelay; //공격 딜레이
+    public float attackDelayA; //공격 활성화 시점
+    public float attackDelayB; //공격 비활성화 시점
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Animator anim; //애니메이터 컨트롤러
+    public BoxCollider boxCollider; //박스 콜라이더
 }
