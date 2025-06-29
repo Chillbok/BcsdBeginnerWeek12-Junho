e8b55ef94c06f76afbdfb2da33a813c836748e37
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-06-29 11:25:29 +0900

    상황에 맞게 손 애니메이션 출력하도록 코드 작성 및 인스펙터 조정 완료

--- Assets/Scripts/HandController.cs
+++ Assets/Scripts/HandController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class HandController : MonoBehaviour
@@ -25,7 +26,53 @@ public class HandController : MonoBehaviour
     {
         if (Input.GetButton("Fire1"))
         {
-
+            if (!isAttack)
+            {
+                //코루틴 실행
+                StartCoroutine(AttackCoroutine());
+            }
         }
+    }
+
+    IEnumerator AttackCoroutine() //공격 관련 시간 조절 코루틴
+    {
+        isAttack = true; //공격 시작
+        currentHand.anim.SetTrigger("Attack");
+
+        yield return new WaitForSeconds(currentHand.attackDelayA);
+        isSwing = true;
+
+        StartCoroutine(HitCoroutine());
+
+        //공격 활성화 시점
+
+        yield return new WaitForSeconds(currentHand.attackDelayB);
+        isSwing = false;
+
+        yield return new WaitForSeconds(currentHand.attackDelay - currentHand.attackDelayA - currentHand.attackDelayB);
+
+        isAttack = false; //공격 종료
+    }
+
+    IEnumerator HitCoroutine() //무언가가 Ray에 맞았는지 맞지 않았는지 판단하는 코루틴
+    {
+        while (isSwing)
+        {
+            if (CheckObject()) //충돌됨
+            {
+                isSwing = false;
+                Debug.Log(hitInfo.transform.name);
+            }
+            yield return null;
+        }
+    }
+
+    private bool CheckObject() //Ray를 이용해서 전방의 적 확인
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out hitInfo, currentHand.range))
+        {
+            return true; //어떤 대상이 충돌했다면
+        }
+        return false; //어떤 대상도 충돌하지 않았다면
     }
 }
