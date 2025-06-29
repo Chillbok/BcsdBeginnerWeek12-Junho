775d9bbaca475eb24416fc77e71bc4d5e80876fd
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-06-29 20:52:06 +0900

    총구 화염 이펙트, 총기 소리 적용 완료

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -6,6 +6,14 @@ public class GunController : MonoBehaviour
 
     private float currentFireRate; //현재 연사 속도. 1초에 1씩 감소하고 0이 되면 발사
 
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     void Update()
     {
         GunFireRateCalc();
@@ -29,11 +37,19 @@ public class GunController : MonoBehaviour
     private void Fire() //발사 과정
     {
         currentFireRate = currentGun.fireRate;
-        Shoot();
+        Shoot(); //총알 발사
     }
 
     private void Shoot() //진짜 총알 쏨
     {
+        currentGun.muzzleFlash.Play(); //총구 화염 효과 재생
+        PlaySE(currentGun.fire_Sound); //발사 소리 출력
         Debug.Log("총알 발사");
+    }
+
+    private void PlaySE(AudioClip _clip) //특정 오디오클립 재생시키는 메소드
+    {
+        audioSource.clip = _clip;
+        audioSource.Play();
     }
 }
