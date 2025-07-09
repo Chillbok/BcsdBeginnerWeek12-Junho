c0602083d145fe7988bbf5b9b297f14ce6f597a0
Chillbok <kylejunho@gmail.com>
2025-07-09 18:37:31 +0900

    돌 스크립트 작성 완료.
    - 일반 상태 돌, 부서진 돌 두 가지 게임 오브젝트 생성
    - 두 가지 돌 게임오브젝트에 리지드바디, 콜라이더 설정

---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    [SerializeField] private int hp; //바위의 체력
+    [SerializeField] private float destroyTime; //파편 제거 시간
+    [SerializeField] private SphereCollider col; //구체 콜라이더
+
+    //필요한 게임 오브젝트
+    [SerializeField] private GameObject go_rock; //일반 바위
+    [SerializeField] private GameObject go_debris; //깨진 바위
+
+    public void Mining()
+    {
+        hp--;
+        if (hp <= 0)
+            Destruction();
+    }
+
+    private void Destruction()
+    {
+        col.enabled = false; //기존 바위 비활성화
+        Destroy(go_rock); //일반 바위 삭제
+
+        go_debris.SetActive(true); //깨진 바위 활성화
+        Destroy(go_debris, destroyTime); //destroyTime만큼 시간 지나면 깨진 바위도 삭제
+    }
+}
